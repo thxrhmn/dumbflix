e5faa1f21ca9b419006a0b5c57cac5dbe43c33a5
thxrhmn <rahmanmuhaemin@gmail.com>
2023-04-22 16:08:39 +0700

    fix

--- server/models/film.go
+++ server/models/film.go
@@ -5,7 +5,7 @@ type Films struct {
 	Title         string   `json:"title" form:"title" gorm:"unique;not null"`
 	Linkfilm      string   `json:"linkfilm" form:"linkfilm" gorm:"type: varchar(255)"`
 	ThumbnailFilm string   `json:"thumbnailfilm" form:"thumbnailfilm" gorm:"type: varchar(255)"`
-	Year          int      `json:"year" form:"year" gorm:"type: varchar(255)"`
+	Year          int      `json:"year" form:"year"`
 	Category      Category `json:"category"`
 	CategoryID    int      `json:"category_id"`
 	Description   string   `json:"description" form:"description" gorm:"type: varchar(255)"`
